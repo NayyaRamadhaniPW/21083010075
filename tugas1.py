19b681cd2e8237259602318039a86e5f474563d6
NayyaRamadhaniPW <21083010075@student.upnjatim.ac.id>
2022-09-11 22:58:51 +0700

    first commit

---
+++ tugas1.py
@@ -0,0 +1,11 @@
+print("-" * 45 )
+print(" Tugas 1 SISOP Kelas-A")
+print("-" * 45)
+print("Nama : Nayya Ramadhani P W")
+print("NPM : 21083010075")
+print("-" * 45 )
+print("Notes")
+print("-" * 45)
+print("Halo dunia ")
+print("Ini adalah skrip Bash pertamaku di Linux")
+
---
+++ tugas1.sh
@@ -0,0 +1,11 @@
+echo "-------------------------------------------"
+echo "Tugas 1 SISOP Kelas-A "
+echo "-------------------------------------------"
+echo "Nama : Nayya Ramadhani P w"
+echo "NPM : 21083010075 "
+echo "-------------------------------------------"
+echo "Notes :"
+echo "-------------------------------------------"
+echo "Halo Dunia "
+echo "Ini adalah skrip Bash pertamaku di Linux"
+
