e7fb4def675cc07119212a2bdc808b50ea4bada3
Nayya Ramadhani Putri Widjanarko <91105782+NayyaRamadhaniPW@users.noreply.github.com>
2022-09-17 14:37:29 +0700

    Delete array.sh

--- array.sh
+++
@@ -1,9 +0,0 @@
-#mendeklarasikan distro 
-distroLinux=("Mint" "Ubuntu" "Kali" "Arch" "Debian")
-
-#random distro 
-let pilih=$RANDOM%5
-
-#eksekusi skrip
-echo "Saya memilih distro $pilih, ${distroLinux[$pilih]} !"
-
