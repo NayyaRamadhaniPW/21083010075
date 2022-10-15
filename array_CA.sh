0a644d4f4c3cc970ad8c9e10ad660ef65ca35efc
Nayya Ramadhani Putri Widjanarko <91105782+NayyaRamadhaniPW@users.noreply.github.com>
2022-10-15 22:55:42 +0700

    Tugas6

---
+++ array_CA.sh
@@ -0,0 +1,6 @@
+distroLinuxDesktop=('BlankOn' 'Ubuntu' 'Debian' 'ArchLinux' 'Linux Mint')
+distroLinuxServer=('UbuntuServer' 'CentOS' 'FedoraServer')
+
+echo ${distroLinuxDesktop[*]}
+echo ${distroLinuxServer[*]}
+
