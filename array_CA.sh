9d6e105053c0eb74aefcfcb5ff2b15294ddf3acc
Nayya Ramadhani Putri Widjanarko <91105782+NayyaRamadhaniPW@users.noreply.github.com>
2022-10-15 22:55:10 +0700

    Delete array_CA.sh

--- array_CA.sh
+++
@@ -1,6 +0,0 @@
-distroLinuxDesktop=('BlankOn' 'Ubuntu' 'Debian' 'ArchLinux' 'Linux Mint')
-distroLinuxServer=('UbuntuServer' 'CentOS' 'FedoraServer')
-
-echo ${distroLinuxDesktop[*]}
-echo ${distroLinuxServer[*]}
-
